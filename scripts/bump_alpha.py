6105c8e1e026f71c2f329b528b63afc1ebafc619
jarbasai <jarbasai@mailfence.com>
2022-02-24 19:42:20 +0000

    fix version bump scripts

--- scripts/bump_alpha.py
+++ scripts/bump_alpha.py
@@ -2,7 +2,7 @@ import fileinput
 from os.path import join, dirname
 
 
-version_file = join(dirname(__file__), "ovos_local_backend", "version.py")
+version_file = join(dirname(dirname(__file__)), "ovos_local_backend", "version.py")
 version_var_name = "VERSION_ALPHA"
 
 with open(version_file, "r", encoding="utf-8") as v:
--- scripts/bump_minor.py
+++ scripts/bump_minor.py
@@ -2,7 +2,7 @@ import fileinput
 from os.path import join, dirname
 
 
-version_file = join(dirname(__file__), "ovos_local_backend", "version.py")
+version_file = join(dirname(dirname(__file__)), "ovos_local_backend", "version.py")
 version_var_name = "VERSION_MINOR"
 build_var_name = "VERSION_BUILD"
 alpha_var_name = "VERSION_ALPHA"
