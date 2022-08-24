8e49ecc44f27d402c59dd03033003345007715cc
jarbasai <jarbasai@mailfence.com>
2022-08-24 20:51:48 +0100

    missing wolfram alpha endpoints

--- ovos_local_backend/backend/external_apis.py
+++ ovos_local_backend/backend/external_apis.py
@@ -60,7 +60,7 @@ def get_services_routes(app):
     @app.route("/" + API_VERSION + '/wolframAlphaSpoken', methods=['GET'])
     @noindex
     @requires_auth
-    def wolfie():
+    def wolfie_spoken():
         # TODO ovos_api support blocked by https://github.com/OpenVoiceOS/ovos_api_service/issues/1
         query = request.args["i"]
         units = request.args.get("units") or _get_units()
@@ -76,6 +76,78 @@ def get_services_routes(app):
         url = 'http://api.wolframalpha.com/v1/spoken'
         params = {"appid": CONFIGURATION["wolfram_key"],
                   "i": query,
+                  "units": units}
+        answer = requests.get(url, params=params).text
+        return answer
+
+    @app.route("/" + API_VERSION + '/wolframAlphaSimple', methods=['GET'])
+    @noindex
+    @requires_auth
+    def wolfie_simple():
+        # TODO ovos_api support blocked by https://github.com/OpenVoiceOS/ovos_api_service/issues/1
+        query = request.args["i"]
+        units = request.args.get("units") or _get_units()
+
+        if units != "metric":
+            units = "imperial"
+
+        # not used?
+        # https://products.wolframalpha.com/spoken-results-api/documentation/
+        # lat, lon = _get_latlon()
+        # geolocation = request.args.get("geolocation") or lat + " " + lon
+
+        url = 'http://api.wolframalpha.com/v1/simple'
+        params = {"appid": CONFIGURATION["wolfram_key"],
+                  "i": query,
+                  "units": units}
+        answer = requests.get(url, params=params).text
+        return answer
+
+    @app.route("/" + API_VERSION + '/wolframAlphaFull', methods=['GET'])
+    @noindex
+    @requires_auth
+    def wolfie_full():
+        # TODO ovos_api support blocked by https://github.com/OpenVoiceOS/ovos_api_service/issues/1
+        query = request.args["i"]
+        units = request.args.get("units") or _get_units()
+
+        if units != "metric":
+            units = "imperial"
+
+        # not used?
+        # https://products.wolframalpha.com/spoken-results-api/documentation/
+        # lat, lon = _get_latlon()
+        # geolocation = request.args.get("geolocation") or lat + " " + lon
+
+        url = 'http://api.wolframalpha.com/v2/query'
+        params = {"appid": CONFIGURATION["wolfram_key"],
+                  "input": query,
+                  "output": "json",
+                  "units": units}
+        answer = requests.get(url, params=params).text
+        return answer
+
+    @app.route("/" + API_VERSION + '/wa', methods=['GET'])
+    @noindex
+    @requires_auth
+    def wolfie_old():
+        """ old deprecated endpoint with XML results """
+        # TODO ovos_api support blocked by https://github.com/OpenVoiceOS/ovos_api_service/issues/1
+        query = request.args["i"]
+        units = request.args.get("units") or _get_units()
+
+        if units != "metric":
+            units = "imperial"
+
+        # not used?
+        # https://products.wolframalpha.com/spoken-results-api/documentation/
+        # lat, lon = _get_latlon()
+        # geolocation = request.args.get("geolocation") or lat + " " + lon
+
+        url = 'http://api.wolframalpha.com/v2/query'
+        params = {"appid": CONFIGURATION["wolfram_key"],
+                  "input": query,
+                  "output": "xml",
                   "units": units}
         answer = requests.get(url, params=params).text
         return answer
