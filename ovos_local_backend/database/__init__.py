8680ae53936d46321260b7e3ee693e80d11b6fa0
jarbasai <jarbasai@mailfence.com>
2023-03-17 23:40:40 +0000

    properties

--- ovos_local_backend/database/__init__.py
+++ ovos_local_backend/database/__init__.py
@@ -33,14 +33,14 @@ class VoiceDefinition(db.Model):
 
 class WakeWordDefinition(db.Model):
     ww_id = db.Column(db.String, primary_key=True)
+    name = db.Column(db.String, nullable=False)
     plugin = db.Column(db.String, nullable=False)  # "module" in mycroft.conf
     ww_cfg = db.Column(db.String, nullable=False)  # arbitrary data for mycroft.conf/OPM
-    # optional metadata
-    fallback_ww_id = db.Column(db.String)  # TODO - link to ww table
 
 
 class Device(db.Model):
     uuid = db.Column(db.String, primary_key=True)
+    token = db.Column(db.String)  # access token, sent to device during pairing
     # device backend preferences
     name = db.Column(db.String)
     placement = db.Column(db.String)  # indoor location
@@ -52,8 +52,68 @@ class Device(db.Model):
     system_unit = db.Column(db.String)
     lang = db.Column(db.String)
     location_json = db.Column(db.String)  # we don't care about querying sub data
-    voice_id = db.Column(db.String)  # TODO - link to voices table
-    ww_id = db.Column(db.String)  # TODO - link to ww table
+
+    voice_id = db.Column(db.String)
+    ww_id = db.Column(db.String)
+
+    @property
+    def selene_device(self):
+        return {
+            "description": self.placement,
+            "uuid": self.uuid,
+            "name": self.name,
+
+            # not tracked / meaningless
+            # just for api compliance with selene
+            'coreVersion': "unknown",
+            'platform': 'unknown',
+            'enclosureVersion': "",
+            "user": {"uuid": self.uuid}  # users not tracked
+        }
+
+    @property
+    def selene_settings(self):
+        # this endpoint corresponds to a mycroft.conf
+        # location is usually grabbed in a separate endpoint
+        # in here we return it in case downstream is
+        # aware of this and wants to save 1 http call
+
+        ww_cfg = {}
+        listener = {}
+        if self.ww_id:
+            ww = db.session.query(WakeWordDefinition).\
+                filter(WakeWordDefinition.ww_id == self.ww_id).first()
+
+            if ww:
+                cfg = json.loads(ww.ww_cfg)
+                ww_cfg = {ww.name: cfg}
+                # NOTE - selene returns the full listener config
+                # this SHOULD NOT be done, since backend has no clue of hardware downstream
+                # we return only wake word config
+                listener = {"wakeWord": ww.name.replace(" ", "_")}
+
+        tts_settings = {}
+        if self.voice_id:
+            voice = db.session.query(VoiceDefinition). \
+                filter(VoiceDefinition.voice_id == self.voice_id).first()
+
+            if voice:
+                cfg = json.loads(voice.voice_cfg)
+                tts_settings = {"module": voice.plugin,
+                                voice.plugin: cfg}
+
+        return {
+            "dateFormat": self.date_fmt,
+           # "optIn": self.opt_in, # backend should not be able to decide this, needs to happen device side
+            "systemUnit": self.system_unit,
+            "timeFormat": self.time_fmt,
+            "uuid": self.uuid,
+            "lang": self.lang,
+            "location": json.loads(self.location_json),
+            "listenerSetting": listener,
+            "hotwordsSetting": ww_cfg,  # not present in selene, parsed correctly by core
+            'ttsSettings': tts_settings
+        }
 
 
 class SkillSettings(db.Model):
@@ -62,6 +122,83 @@ class SkillSettings(db.Model):
     display_name = db.Column(db.String)  # for friendly UI, default to skill_id
     settings_json = db.Column(db.String, nullable=False)  # actual skill settings file
     metadata_json = db.Column(db.String, nullable=False)  # how to display user facing settings editor
+
+    def serialize(self):
+        # settings meta with updated placeholder values from settings
+        # old style selene db stored skill settings this way
+        meta = json.loads(self.metadata_json)
+        settings = json.loads(self.settings_json)
+        for idx, section in enumerate(meta.get('sections', [])):
+            for idx2, field in enumerate(section["fields"]):
+                if "value" not in field:
+                    continue
+                if field["name"] in settings:
+                    meta['sections'][idx]["fields"][idx2]["value"] = settings[field["name"]]
+        return {'skillMetadata': meta,
+                "skill_gid": self.remote_id,
+                "display_name": self.display_name}
+
+    @staticmethod
+    def deserialize(data):
+        if isinstance(data, str):
+            data = json.loads(data)
+
+        skill_json = {}
+        skill_meta = data.get("skillMetadata") or {}
+        for s in skill_meta.get("sections", []):
+            for f in s.get("fields", []):
+                if "name" in f and "value" in f:
+                    val = f["value"]
+                    if isinstance(val, str):
+                        t = f.get("type", "")
+                        if t == "checkbox":
+                            if val.lower() == "true" or val == "1":
+                                val = True
+                            else:
+                                val = False
+                        elif t == "number":
+                            if val == "False":
+                                val = 0
+                            elif val == "True":
+                                val = 1
+                            else:
+                                val = float(val)
+                        elif val.lower() in ["none", "null", "nan"]:
+                            val = None
+                        elif val == "[]":
+                            val = []
+                        elif val == "{}":
+                            val = {}
+                    skill_json[f["name"]] = val
+
+        remote_id = data.get("skill_gid") or data.get("identifier")
+        # this is a mess, possible keys seen by logging data
+        # - @|XXX
+        # - @{uuid}|XXX
+        # - XXX
+
+        # where XXX has been observed to be
+        # - {skill_id}  <- ovos-core
+        # - {msm_name} <- mycroft-core
+        #   - {mycroft_marketplace_name} <- all default skills
+        #   - {MycroftSkill.name} <- sometimes sent to msm (very uncommon)
+        #   - {skill_id.split(".")[0]} <- fallback msm name
+        # - XXX|{branch} <- append by msm (?)
+        # - {whatever we feel like uploading} <- SeleneCloud utils
+        fields = remote_id.split("|")
+        skill_id = fields[0]
+        if len(fields) > 1 and fields[0].startswith("@"):
+            skill_id = fields[1]
+
+        display_name = data.get("display_name") or \
+                       skill_id.split(".")[0].replace("-", " ").replace("_", " ").title()
+
+        settings_json = json.dumps(skill_json)
+        metadata_json = json.dumps(skill_meta)
+        return SkillSettings(settings_json=settings_json,
+                             metadata_json=metadata_json,
+                             display_name=display_name,
+                             remote_id=remote_id)
 
 
 class Metric(db.Model):
