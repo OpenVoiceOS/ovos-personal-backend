32229eff33aebe47d123ccae0000f9d449f5271a
jarbasai <jarbasai@mailfence.com>
2023-03-30 22:16:44 +0100

    stub - CRUD endpoints

--- ovos_local_backend/backend/admin.py
+++ ovos_local_backend/backend/admin.py
@@ -13,13 +13,227 @@
 import time
 
 from flask import request
-
 from ovos_local_backend.backend import API_VERSION
 from ovos_local_backend.backend.decorators import noindex, requires_admin
 from ovos_local_backend.database import add_device, update_device
 from ovos_local_backend.utils import generate_code
 from ovos_local_backend.utils import nice_json
 from ovos_local_backend.utils.geolocate import get_request_location
+
+
+def get_database_crud(app):
+    # DATABASE - (backend manager uses these)
+    @app.route("/" + API_VERSION + "/admin/<uuid>/skill_settings",
+               methods=['POST'])
+    @requires_admin
+    @noindex
+    def create_skill_settings(uuid):
+        return {}  # TODO
+
+    @app.route("/" + API_VERSION + "/admin/<uuid>/skill_settings/list",
+               methods=['GET'])
+    @requires_admin
+    @noindex
+    def list_skill_settings(uuid):
+        return {}  # TODO
+
+    @app.route("/" + API_VERSION + "/admin/<uuid>/skill_settings/<skill_id>",
+               methods=['GET', "PUT", "DELETE"])
+    @requires_admin
+    @noindex
+    def get_skill_settings(uuid, skill_id):
+        return {}  # TODO
+
+    @app.route("/" + API_VERSION + "/admin/skill_settings",
+               methods=['POST'])
+    @requires_admin
+    @noindex
+    def create_shared_skill_settings():
+        return {}  # TODO
+
+    @app.route("/" + API_VERSION + "/admin/skill_settings/list",
+               methods=['GET'])
+    @requires_admin
+    @noindex
+    def list_shared_skill_settings():
+        return {}  # TODO
+
+    @app.route("/" + API_VERSION + "/admin/skill_settings/<skill_id>",
+               methods=['GET', "PUT", "DELETE"])
+    @requires_admin
+    @noindex
+    def get_shared_skill_settings(skill_id):
+        return {}  # TODO
+
+    @app.route("/" + API_VERSION + "/admin/oauth_apps",
+               methods=['POST'])
+    @requires_admin
+    @noindex
+    def create_oauth_app(uuid):
+        return {}  # TODO
+
+    @app.route("/" + API_VERSION + "/admin/oauth_apps/list",
+               methods=['GET'])
+    @requires_admin
+    @noindex
+    def list_oauth_apps(uuid):
+        return {}  # TODO
+
+    @app.route("/" + API_VERSION + "/admin/oauth_apps/<uuid>",
+               methods=['GET', "PUT", "DELETE"])
+    @requires_admin
+    @noindex
+    def get_oauth_apps(uuid):
+        return {}  # TODO
+
+    @app.route("/" + API_VERSION + "/admin/oauth_toks",
+               methods=['POST'])
+    @requires_admin
+    @noindex
+    def create_oauth_toks():
+        return {}  # TODO
+
+    @app.route("/" + API_VERSION + "/admin/oauth_toks/list",
+               methods=['GET'])
+    @requires_admin
+    @noindex
+    def list_oauth_toks():
+        return {}  # TODO
+
+    @app.route("/" + API_VERSION + "/admin/oauth_toks/<uuid>",
+               methods=['GET', "PUT", "DELETE"])
+    @requires_admin
+    @noindex
+    def get_oauth_toks(uuid):
+        return {}  # TODO
+
+    @app.route("/" + API_VERSION + "/admin/voice_recs",
+               methods=['POST'])
+    @requires_admin
+    @noindex
+    def create_voice_rec():
+        return {}  # TODO
+
+    @app.route("/" + API_VERSION + "/admin/voice_recs/list",
+               methods=['GET'])
+    @requires_admin
+    @noindex
+    def list_voice_recs():
+        return {}  # TODO
+
+    @app.route("/" + API_VERSION + "/admin/voice_recs/<uuid>",
+               methods=['GET', "PUT", "DELETE"])
+    @requires_admin
+    @noindex
+    def get_voice_recs(uuid):
+        return {}  # TODO
+
+    @app.route("/" + API_VERSION + "/admin/ww_recs",
+               methods=['POST'])
+    @requires_admin
+    @noindex
+    def create_ww_rec():
+        return {}  # TODO
+
+    @app.route("/" + API_VERSION + "/admin/ww_recs/list",
+               methods=['GET'])
+    @requires_admin
+    @noindex
+    def list_ww_recs():
+        return {}  # TODO
+
+    @app.route("/" + API_VERSION + "/admin/ww_recs/<uuid>",
+               methods=['GET', "PUT", "DELETE"])
+    @requires_admin
+    @noindex
+    def get_ww_recs(uuid):
+        return {}  # TODO
+
+    @app.route("/" + API_VERSION + "/admin/metrics",
+               methods=['POST'])
+    @requires_admin
+    @noindex
+    def create_metric():
+        return {}  # TODO
+
+    @app.route("/" + API_VERSION + "/admin/metrics/list",
+               methods=['GET'])
+    @requires_admin
+    @noindex
+    def list_metrics():
+        return {}  # TODO
+
+    @app.route("/" + API_VERSION + "/admin/metrics/<uuid>",
+               methods=['GET', "PUT", "DELETE"])
+    @requires_admin
+    @noindex
+    def get_metric(uuid):
+        return {}  # TODO
+
+    @app.route("/" + API_VERSION + "/admin/devices",
+               methods=['POST'])
+    @requires_admin
+    @noindex
+    def create_device():
+        return {}  # TODO
+
+    @app.route("/" + API_VERSION + "/admin/devices/list",
+               methods=['GET'])
+    @requires_admin
+    @noindex
+    def list_devices():
+        return {}  # TODO
+
+    @app.route("/" + API_VERSION + "/admin/device/<uuid>",
+               methods=['GET', "PUT", "DELETE"])
+    @requires_admin
+    @noindex
+    def get_device(uuid):
+        return {}  # TODO
+
+    @app.route("/" + API_VERSION + "/admin/voice_defs",
+               methods=['POST'])
+    @requires_admin
+    @noindex
+    def create_voice_defs():
+        return {}  # TODO
+
+    @app.route("/" + API_VERSION + "/admin/voice_defs/list",
+               methods=['GET'])
+    @requires_admin
+    @noindex
+    def list_voice_defs():
+        return {}  # TODO
+
+    @app.route("/" + API_VERSION + "/admin/voice_defs/<voice_id>",
+               methods=['GET', "PUT", "DELETE"])
+    @requires_admin
+    @noindex
+    def get_voice_def(voice_id):
+        return {}  # TODO
+
+    @app.route("/" + API_VERSION + "/admin/ww_defs",
+               methods=['POST'])
+    @requires_admin
+    @noindex
+    def create_ww_def():
+        return {}  # TODO
+
+    @app.route("/" + API_VERSION + "/admin/ww_defs/list",
+               methods=['GET'])
+    @requires_admin
+    @noindex
+    def list_ww_defs():
+        return {}  # TODO
+
+    @app.route("/" + API_VERSION + "/admin/ww_defs/<ww_id>",
+               methods=['GET', "PUT", "DELETE"])
+    @requires_admin
+    @noindex
+    def get_ww_def(ww_id):
+        return {}  # TODO
+
+    return app
 
 
 def get_admin_routes(app):
@@ -60,4 +274,4 @@ def get_admin_routes(app):
         device_data = update_device(uuid, **request.json)
         return nice_json(device_data)
 
-    return app
+    return get_database_crud(app)
