8b089ff090a4a3194d83c95a47700042fc3edf05
generallynonsensical <112360998+generallynonsensical@users.noreply.github.com>
2023-05-13 13:28:34 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -52,13 +52,14 @@ setup(
     name='ovos-local-backend',
     version=get_version(),
     packages=['ovos_local_backend',
-              'ovos_local_backend.utils',
-              'ovos_local_backend.backend'],
+              'ovos_local_backend.backend',
+              'ovos_local_backend.utils']
+            
     install_requires=required("requirements/requirements.txt"),
     extras_requires={
         "mysql": ["Flask-MySQLdb", "mysqlclient"]
     },
-    package_data={'': package_files('ovos_local_backend')},
+    package_data={'': package_files('ovos_local_backend/backend, ovos_local_backend/utils')},
     include_package_data=True,
     url='https://github.com/OpenVoiceOS/OVOS-local-backend',
     license='Apache-2.0',
