71494ec10c3a51f032833874ee460e36579cd903
JarbasAi <jarbasai@mailfence.com>
2023-06-08 23:51:03 +0100

    feat/admin_get_backend_config

--- ovos_local_backend/backend/admin.py
+++ ovos_local_backend/backend/admin.py
@@ -20,15 +20,17 @@ from ovos_local_backend.backend.decorators import noindex, requires_admin
 from ovos_local_backend.utils import generate_code
 from ovos_local_backend.utils import nice_json
 from ovos_local_backend.utils.geolocate import get_request_location
-from ovos_config import LocalConf, USER_CONFIG
+from ovos_config import LocalConf, USER_CONFIG, Configuration
 
 
 def get_admin_routes(app):
 
-    @app.route("/" + API_VERSION + "/admin/config", methods=['POST'])
+    @app.route("/" + API_VERSION + "/admin/config", methods=['POST', 'GET'])
     @requires_admin
     @noindex
     def update_config():
+        if flask.request.method == 'GET':
+            return nice_json(Configuration())
         cfg = LocalConf(USER_CONFIG)
         cfg.merge(flask.request.json["config"])
         cfg.store()
