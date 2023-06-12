6eaa96bb5e9baf383042c2b5d1e348a6a8003ba6
JarbasAI <33701864+JarbasAl@users.noreply.github.com>
2023-06-12 18:27:35 +0100

    Update ovos.conf
    add base_folder to ovos.conf

--- ovos_local_backend/__init__.py
+++ ovos_local_backend/__init__.py
@@ -5,6 +5,7 @@ from ovos_config.utils import init_module_config
 init_module_config("ovos_local_backend",
                    "ovos_local_backend",
                    {"config_filename": "ovos_backend.conf",
+                    "base_folder" :"ovos_backend",
                     "default_config_path": f"{os.path.dirname(__file__)}/ovos_backend.conf"})
 
 from ovos_local_backend.backend import start_backend
