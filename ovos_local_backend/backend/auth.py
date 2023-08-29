a546cb15883bf61818ca552e3c1cf4e1d7373160
Åke <ake.forslund@gmail.com>
2023-08-29 03:05:11 +0200

    Oauth fixes (#71)

--- ovos_local_backend/backend/auth.py
+++ ovos_local_backend/backend/auth.py
@@ -20,7 +20,7 @@ from oauthlib.oauth2 import WebApplicationClient
 
 from ovos_local_backend.backend import API_VERSION
 from ovos_local_backend.backend.decorators import noindex, requires_auth
-from ovos_local_backend.database import add_oauth_application, add_oauth_token, get_oauth_application, get_oauth_token
+from ovos_local_backend.database import update_oauth_application, add_oauth_token, get_oauth_application, get_oauth_token
 
 from ovos_local_backend.utils import nice_json
 
@@ -47,32 +47,34 @@ def get_auth_routes(app):
     @app.route(f"/{API_VERSION}/auth/<oauth_id>/auth_url", methods=['GET'])
     @requires_auth
     @noindex
-    def oauth_url(oauth_id):
+    def register_oauth_application(oauth_id):
         """ send auth url to user to confirm authorization,
         once user opens it callback is triggered
         """
         auth = flask.request.headers.get('Authorization', '').replace("Bearer ", "")
         uid = auth.split(":")[-1]
-        token_id = f"{uid}|{oauth_id}"
+        if uid != '':
+            token_id = f"{uid}|{oauth_id}"
+        else:
+            token_id = oauth_id
 
         params = dict(flask.request.args)
-        params["callback_endpoint"] = flask.request.base_url + f"/{API_VERSION}/auth/callback/{token_id}"
-
+        callback_endpoint = f"{flask.request.host_url}{API_VERSION}/auth/callback/{token_id}"
         client = WebApplicationClient(params["client_id"])
         request_uri = client.prepare_request_uri(
             params["auth_endpoint"],
-            redirect_uri=params["callback_endpoint"],
+            redirect_uri=callback_endpoint,
             scope=params["scope"],
         )
 
-        add_oauth_application(token_id=token_id,
-                               client_id=params["client_id"],
-                               client_secret=params["client_secret"],
-                               auth_endpoint=params["auth_endpoint"],
-                               token_endpoint=params["token_endpoint"],
-                               refresh_endpoint=params["refresh_endpoint"],
-                               callback_endpoint=params["callback_endpoint"],
-                               scope=params["scope"])
+        update_oauth_application(token_id=token_id,
+                                 client_id=params["client_id"],
+                                 client_secret=params["client_secret"],
+                                 auth_endpoint=params["auth_endpoint"],
+                                 token_endpoint=params["token_endpoint"],
+                                 refresh_endpoint=params["refresh_endpoint"],
+                                 callback_endpoint=callback_endpoint,
+                              scope=params["scope"])
 
         return request_uri, 200
 
@@ -85,9 +87,9 @@ def get_auth_routes(app):
         code = params["code"]
 
         data = get_oauth_application(token_id)
-        client_id = data["client_id"]
-        client_secret = data["client_secret"]
-        token_endpoint = data["token_endpoint"]
+        client_id = data.client_id
+        client_secret = data.client_secret
+        token_endpoint = data.token_endpoint
 
         # Prepare and send a request to get tokens! Yay tokens!
         client = WebApplicationClient(client_id)
